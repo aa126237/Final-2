9360b0d7fb578ee40125feb9dfde9e5468e3be55
Alex Dawson <aad4wson@gmail.com>
2019-12-17 20:01:00 -0600

    moving character

--- Final/Final/Form1.Designer.cs
+++ Final/Final/Form1.Designer.cs
@@ -28,33 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.components = new System.ComponentModel.Container();
+            this.pb_Ground = new System.Windows.Forms.PictureBox();
+            this.pb_Player = new System.Windows.Forms.PictureBox();
+            this.tmr_Gravity = new System.Windows.Forms.Timer(this.components);
+            this.tmr_Up = new System.Windows.Forms.Timer(this.components);
+            this.tmr_Right = new System.Windows.Forms.Timer(this.components);
+            this.tmr_Left = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Ground)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Player)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // pb_Ground
             // 
-            this.button1.Location = new System.Drawing.Point(300, 111);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(135, 48);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Start Game";
-            this.button1.UseVisualStyleBackColor = true;
+            this.pb_Ground.BackColor = System.Drawing.Color.Green;
+            this.pb_Ground.Location = new System.Drawing.Point(-8, 409);
+            this.pb_Ground.Name = "pb_Ground";
+            this.pb_Ground.Size = new System.Drawing.Size(806, 62);
+            this.pb_Ground.TabIndex = 0;
+            this.pb_Ground.TabStop = false;
+            // 
+            // pb_Player
+            // 
+            this.pb_Player.BackColor = System.Drawing.Color.Red;
+            this.pb_Player.Location = new System.Drawing.Point(354, 181);
+            this.pb_Player.Name = "pb_Player";
+            this.pb_Player.Size = new System.Drawing.Size(51, 50);
+            this.pb_Player.TabIndex = 1;
+            this.pb_Player.TabStop = false;
+            // 
+            // tmr_Gravity
+            // 
+            this.tmr_Gravity.Enabled = true;
+            this.tmr_Gravity.Interval = 10;
+            this.tmr_Gravity.Tick += new System.EventHandler(this.tmr_Gravity_Tick);
+            // 
+            // tmr_Up
+            // 
+            this.tmr_Up.Interval = 10;
+            this.tmr_Up.Tick += new System.EventHandler(this.tmr_Up_Tick);
+            // 
+            // tmr_Right
+            // 
+            this.tmr_Right.Interval = 10;
+            this.tmr_Right.Tick += new System.EventHandler(this.tmr_Right_Tick);
+            // 
+            // tmr_Left
+            // 
+            this.tmr_Left.Interval = 10;
+            this.tmr_Left.Tick += new System.EventHandler(this.tmr_Left_Tick);
             // 
             // The_Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 461);
-            this.Controls.Add(this.button1);
+            this.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.ClientSize = new System.Drawing.Size(784, 462);
+            this.Controls.Add(this.pb_Player);
+            this.Controls.Add(this.pb_Ground);
             this.Name = "The_Form";
             this.Text = "Game";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.The_Form_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.The_Form_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Ground)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Player)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pb_Ground;
+        private System.Windows.Forms.PictureBox pb_Player;
+        private System.Windows.Forms.Timer tmr_Gravity;
+        private System.Windows.Forms.Timer tmr_Up;
+        private System.Windows.Forms.Timer tmr_Right;
+        private System.Windows.Forms.Timer tmr_Left;
     }
 }
 
--- Final/Final/Form1.cs
+++ Final/Final/Form1.cs
@@ -12,9 +12,69 @@ namespace Final
 {
     public partial class The_Form : Form
     {
+
+        bool isJumping = false;
+
         public The_Form()
         {
             InitializeComponent();
+        }
+
+        private void tmr_Gravity_Tick(object sender, EventArgs e)
+        {
+            if(!pb_Player.Bounds.IntersectsWith(pb_Ground.Bounds) && isJumping == false)
+            {
+                pb_Player.Top += 10;
+            }
+        }
+
+        private void tmr_Up_Tick(object sender, EventArgs e)
+        {
+            pb_Player.Top -= 10;
+            isJumping = true;
+        }
+
+        private void tmr_Right_Tick(object sender, EventArgs e)
+        {
+            pb_Player.Left += 10;
+        }
+
+        private void tmr_Left_Tick(object sender, EventArgs e)
+        {
+            pb_Player.Left -= 10;
+        }
+
+        private void The_Form_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Up)
+            {
+                tmr_Up.Start();
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                tmr_Right.Start();
+            }
+            else if (e.KeyCode == Keys.Left)
+            {
+                tmr_Left.Start();
+            }
+        }
+
+        private void The_Form_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                tmr_Up.Stop();
+                isJumping = false;
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                tmr_Right.Stop();
+            }
+            else if (e.KeyCode == Keys.Left)
+            {
+                tmr_Left.Stop();
+            }
         }
     }
 }
